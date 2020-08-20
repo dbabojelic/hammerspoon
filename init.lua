276f8969f746704c7354deaf27f14a7322e204a2
dbabojelic <dario.bab27@gmail.com>
2020-08-20 12:07:51 +0200

    initial config

---
+++ init.lua
@@ -0,0 +1,155 @@
+-- CONFIG RELOAD
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
+
+hs.notify.new({title="Hammerspoon", informativeText="Config loaded"}):send()
+
+
+-- INSTALL HYPER KEY
+local hyper = require('hyper')
+hyper.install('F18')
+
+-- WIN MANAGEMENT
+hs.window.animationDuration = 0
+hs.loadSpoon("WinWin")
+
+
+
+-- focus left, right, up, down
+hyper.bindKey("H", function() 
+	hs.window.filter.focusWest()
+end)
+hyper.bindKey("K", function() 
+	hs.window.filter.focusNorth()
+end)
+hyper.bindKey("J", function() 
+	hs.window.filter.focusSouth()
+end)
+hyper.bindKey("L", function() 
+	hs.window.filter.focusEast()
+end)
+
+-- move left, right, up, down
+hyper.bindShiftKey("H", function() 
+	spoon.WinWin:moveAndResize("halfleft")
+end)
+hyper.bindShiftKey("K", function() 
+	spoon.WinWin:moveAndResize("halfup")
+end)
+hyper.bindShiftKey("J", function() 
+	spoon.WinWin:moveAndResize("halfdown")
+end)
+hyper.bindShiftKey("L", function() 
+	spoon.WinWin:moveAndResize("halfright")
+end)
+
+-- 4x4 grid
+hyper.bindKey("1", function() 
+	spoon.WinWin:moveAndResize("cornerNW")
+end)
+hyper.bindKey("2", function() 
+	spoon.WinWin:moveAndResize("cornerNE")
+end)
+hyper.bindKey("3", function() 
+	spoon.WinWin:moveAndResize("cornerSW")
+end)
+hyper.bindKey("4", function() 
+	spoon.WinWin:moveAndResize("cornerSE")
+end)
+
+-- fullscreen
+hyper.bindKey("F", function()
+  local win = hs.window.focusedWindow();
+  win:toggleFullScreen()
+end)
+
+
+-- moving to another screen
+hyper.bindKey("B", function()
+	local win = hs.window.focusedWindow();
+	win:moveToScreen(win:screen():next())
+end)
+
+-- minimize
+hyper.bindKey("M", function()
+  local win = hs.window.focusedWindow();
+  win:maximize()
+end)
+
+
+
+
+-- APPLICATION CHOOSER
+
+-- in progress
+myLaunchOrFocus = function(appName) 
+	local win = hs.window.focusedWindow();
+	local app = win:application();
+
+	hs.notify.new({title="appName!", informativeText=appName}):send()
+	hs.notify.new({title="app:name!", informativeText=app:name()}):send()
+	if app:name() == appName then
+		local wf = hs.window.filter
+		wf_app = wf.new{app:name()}
+		wins = wf_app:getWindows(hs.window.filter.sortByFocused)
+		wins[2]:focus()
+		hs.notify.new({title="OK!", informativeText=wins[1]:title()}):send()
+	else
+		hs.notify.new({title="NOT OK!", informativeText="bla"}):send()
+		if appName ~= "iTerm2" then
+			hs.application.launchOrFocus(appName)
+		else
+			hs.application.launchOrFocus("iTerm")
+		end
+	end
+end
+
+-- moras bas stavit puna imena, prava imena
+local applicationHotkeys = {
+  w = 'Firefox',
+  i = 'iTerm',
+  e = 'Mail',
+  s = 'System Preferences',
+  n = 'Notes',
+  v = 'MacVim',
+}
+
+for key, app in pairs(applicationHotkeys) do
+  hyper.bindKey(key, function()
+	  hs.application.launchOrFocus(app)
+	  --myLaunchOrFocus(app)
+  end)
+end
+
+
+-- SLEEP and LOCK 
+hyper.bindShiftKey("S", function() hs.caffeinate.systemSleep() end)
+hyper.bindShiftKey("9", function() hs.caffeinate.lockScreen() end)
+
+-- MUTE ON WAKE
+function muteOnWake(eventType)
+  if (eventType == hs.caffeinate.watcher.systemDidWake) then
+    local output = hs.audiodevice.defaultOutputDevice()
+    output:setMuted(true)
+  end
+end
+caffeinateWatcher = hs.caffeinate.watcher.new(muteOnWake)
+caffeinateWatcher:start()
+
+
+-- MOVE BETWEEN SPACES
+require('spaces')
+hyper.bindShiftKey("left", function() moveWindowOneSpace("left", true) end)
+hyper.bindShiftKey("right", function() moveWindowOneSpace("right", true) end)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ spaces.lua
@@ -0,0 +1,49 @@
+-- Code from https://github.com/Hammerspoon/hammerspoon/issues/235
+-- Make sure to install spaces module https://github.com/asmagill/hs._asm.undocumented.spaces
+
+local hotkey = require "hs.hotkey"
+local window = require "hs.window"
+local spaces = require "hs._asm.undocumented.spaces" 
+
+local function getGoodFocusedWindow(nofull)
+   local win = window.focusedWindow()
+   if not win or not win:isStandard() then return end
+   if nofull and win:isFullScreen() then return end
+   return win
+end 
+
+
+
+--local logger = hs.logger.new("mylog", "debug")
+
+function moveWindowOneSpace(dir,switch)
+   local win = getGoodFocusedWindow(true)
+   if not win then return end
+   local screen=win:screen()
+   local uuid=screen:spacesUUID()
+   local userSpaces=spaces.layout()[uuid]
+   local thisSpace=win:spaces() -- first space win appears on
+   if not thisSpace then return else thisSpace=thisSpace[1] end
+   local last=nil
+   local skipSpaces=0
+   for _, spc in ipairs(userSpaces) do
+      if spaces.spaceType(spc)~=spaces.types.user then -- skippable space
+	 skipSpaces=skipSpaces+1
+      else 			-- A good user space, check it
+	 if last and
+	    (dir=="left"  and spc==thisSpace) or
+	    (dir=="right" and last==thisSpace)
+	 then
+	    win:spacesMoveTo(dir=="left" and last or spc)
+	    if switch then
+	       win:focus()
+	    end
+	    return
+	 end
+	 last=spc	 -- Haven't found it yet...
+	 skipSpaces=0
+      end 
+   end
+end
+
+
